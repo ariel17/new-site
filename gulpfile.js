b6ec1748441d83af834993c0f1f071125269cca6
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2020-10-13 00:16:15 -0300

    Using FA CSS directly.

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,22 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const del = require('del');
+const rename = require('gulp-rename');
 
 function clean() {
-     return del('public/**', {force:true});
+    return del(['public/**'], {force:true});
 }
 
 function style() {
-    return gulp.src('scss/**/*.scss').
+    return gulp.src('scss/*.scss').
         pipe(sass().on('error', sass.logError)).
         pipe(gulp.dest('public'));
 }
 
-function webfonts() {
-    return gulp.src('./node_modules/@fortawesome/fontawesome-free/webfonts/**/*').
-        pipe(gulp.dest('public'));
+function fa() {
+    return gulp.src([
+        './node_modules/@fortawesome/fontawesome-free/**/*',
+    ]).pipe(gulp.dest('public/fontawesome'));
 }
 
-exports.default = gulp.series(clean, style, webfonts);
+exports.default = gulp.series(clean, fa, style);
