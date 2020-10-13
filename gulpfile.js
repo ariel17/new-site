8c81085123a7b932c150132a73d16bed90b74bbc
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2020-10-13 00:22:16 -0300

    Changes to include FA in SCSS files.

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,30 @@ const del = require('del');
 const rename = require('gulp-rename');
 
 function clean() {
-    return del(['public/**'], {force:true});
+    return del(['public/**', 'scss/fontawesome'], {force:true});
 }
 
 function style() {
-    return gulp.src('scss/*.scss').
+    return gulp.src('scss/custom.scss').
         pipe(sass().on('error', sass.logError)).
         pipe(gulp.dest('public'));
 }
 
-function fa() {
-    return gulp.src([
-        './node_modules/@fortawesome/fontawesome-free/**/*',
-    ]).pipe(gulp.dest('public/fontawesome'));
+function _fa() {
+    return gulp.src('./node_modules/@fortawesome/fontawesome-free/scss/*').
+        pipe(gulp.dest('scss/fontawesome'));
 }
 
-exports.default = gulp.series(clean, fa, style);
+function _fa_variables() {
+    del('scss/fontawesome/_variables.scss', {force:true});
+    return gulp.src('scss/fa_variables.scss').
+        pipe(rename('_variables.scss')).
+        pipe(gulp.dest('scss/fontawesome'));
+}
+
+function _fa_webfonts() {
+    return gulp.src('./node_modules/@fortawesome/fontawesome-free/webfonts/*').
+        pipe(gulp.dest('public/webfonts'));
+}
+
+exports.default = gulp.series(clean, _fa, _fa_variables, _fa_webfonts, style);
